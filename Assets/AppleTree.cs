3e9596d759b655895ef55c149cd44a5c8e487d29
Hypertonick02 <nsm140@scarletmail.rutgers.edu>
2025-09-17 23:05:03 -0400

    Added scripts and physics layers to game objects

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -12,12 +12,33 @@ public class AppleTree : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Invoke ("DropApple", 2f);
+    }
+
+    void DropApple(){
+        GameObject Apple = Instantiate<GameObject>( applePrefab );
+        Apple.transform.position = transform.position;
+        Invoke( "DropApple", appleDropDelay );
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 pos =transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x < -leftAndRightEdge){
+            speed = Mathf.Abs(speed);
+        }
+        else if (pos.x > leftAndRightEdge){
+            speed = -Mathf.Abs(speed);
+        }
         
+    }
+    void FixedUpdate(){
+        if (Random.value < changeDirChance){
+            speed *= -1;
+        }
     }
 }
